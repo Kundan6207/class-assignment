21650d1609f5ce4ee22829ed2cd503b25760e8ef
kundan6207 <kundank20222@gmail.com>
2023-01-13 08:16:41 +0530

    assignment-3

---
+++ Q.NO-5.js
@@ -0,0 +1,15 @@
+const arr5 = new Array();
+arr5.push(1);
+arr5.push(3);
+arr5.push(5);
+arr5.push(3);
+arr5.push(4);
+arr5.push(5);
+arr5.push(80);
+console.log(arr5);
+
+arr5.sort();
+console.log(arr5);
+
+arr5.pop();
+console.log(arr5);
---
+++ Q.NO-7.js
@@ -0,0 +1,15 @@
+// number
+x = 455;
+console.log("Value of x=" + x);
+
+// string
+str = "Hello world";
+console.log("Value of str=" + str);
+
+// boolean
+b = true;
+console.log("value of bool=" + b);
+
+// null -> intentional absence of any object value
+x = null;
+console.log("Value of x=" + x);
